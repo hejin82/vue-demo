3d9d18b93ec233d401be438be0bb1a445b7aef74
hejin <hejin82@gmail.com>
2020-01-10 00:26:47 +0900

    complete todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -4,13 +4,44 @@
       {{ name }}'s To Do List
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do.</b>
+        </div>
       </div>
-      <div class="row" v-for="task in tasks" v-bind:key="task.action">
-        <div class="col">{{ task.action }}</div>
-        <div class="col-2">{{ task.done }}</div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="task in filteredTasks" v-bind:key="task.action">
+          <div class="col">{{ task.action }}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="task.done" class="form-check-input" />
+            {{ task.done }}
+          </div>
+        </div>
+      </template>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete completed
+          </button>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
       </div>
     </div>
   </div>
@@ -22,8 +53,38 @@ export default {
   data() {
     return {
       name: "Adam",
-      tasks: [{ action: "buy flowers", done: false }]
+      tasks: [],
+      hideCompleted: true,
+      newItemText: ""
     };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+      this.storeData();
+      this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      this.tasks = JSON.parse(data);
+    }
   }
 };
 </script>

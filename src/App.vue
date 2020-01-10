41eb050452dad62bc08a2fc56831fbbca60a4100
hejin <hejin82@gmail.com>
2020-01-11 01:24:21 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,19 @@
 <script>
 // import Todo from "./components/Todo.vue";
 import Store from "./components/Store.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "app",
   components: {
     // Todo,
     Store
+  },
+  methods: {
+    ...mapActions(["getData"])
+  },
+  created() {
+    this.getData();
   }
 };
 </script>

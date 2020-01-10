9a619429a588fbf8cbb9c4f084ca51c585133cd8
hejin <hejin82@gmail.com>
2020-01-10 21:31:54 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Todo />
+    <!-- <Todo /> -->
+    <Store />
   </div>
 </template>
 
 <script>
-import Todo from "./components/Todo.vue";
+// import Todo from "./components/Todo.vue";
+import Store from "./components/Store.vue";
 
 export default {
   name: "app",
   components: {
-    Todo
+    // Todo,
+    Store
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+import store from "./store";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount("#app");

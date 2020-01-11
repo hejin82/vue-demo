3499e3ba0ba4adb600df6015b2e62ec08d844056
hejin <hejin82@gmail.com>
2020-01-11 22:14:55 +0900

    commit

--- src/components/store/PageControls.vue
+++ src/components/store/PageControls.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="row mt-2">
-    <div class="col form-group">
+    <div class="col-3 form-group">
       <select class="form-control" v-on:change="changePageSize">
         <option value="4">4 per page</option>
         <option value="8">8 per page</option>
         <option value="12">12 per page</option>
       </select>
     </div>
-    <div v-if="pageCount > 1" class=" col text-right">
-      <div class="btn-group mx-2">
+    <div class=" col text-right">
+      <button
+        v-bind:disabled="currentPage == 1"
+        v-on:click="setCurrentPage(currentPage - 1)"
+        class="btn btn-secondary mx-1"
+      >
+        Previous
+      </button>
+      <span v-if="currentPage > 4">
+        <button v-on:click="setCurrentPage(1)" class="btn btn-secondary mx-1">
+          1
+        </button>
+        <span class="h4">...</span>
+      </span>
+      <span class="mx-1">
         <button
           v-for="i in pageNumbers"
           v-bind:key="i"
@@ -18,7 +31,23 @@
         >
           {{ i }}
         </button>
-      </div>
+      </span>
+      <span v-if="currentPage <= pageCount - 4">
+        <span class="h4">...</span>
+        <button
+          v-on:click="setCurrentPage(pageCount)"
+          class="btn btn-secondary mx-1"
+        >
+          {{ pageCount }}
+        </button>
+      </span>
+      <button
+        v-bind:disabled="currentPage == pageCount"
+        v-on:click="setCurrentPage(currentPage + 1)"
+        class="btn btn-secondary mx-1"
+      >
+        Next
+      </button>
     </div>
   </div>
 </template>
@@ -29,7 +58,15 @@ export default {
     ...mapState(["currentPage"]),
     ...mapGetters(["pageCount"]),
     pageNumbers() {
-      return [...Array(this.pageCount + 1).keys()].slice(1);
+      if (this.pageCount < 4) {
+        return [...Array(this.pageCount + 1).keys()].splice(1);
+      } else if (this.currentPage <= 4) {
+        return [1, 2, 3, 4, 5];
+      } else if (this.currentPage > this.pageCount - 4) {
+        return [...Array(5).keys()].reverse().map(v => this.pageCount - v);
+      } else {
+        return [this.currentPage - 1, this.currentPage, this.currentPage + 1];
+      }
     }
   },
   methods: {
